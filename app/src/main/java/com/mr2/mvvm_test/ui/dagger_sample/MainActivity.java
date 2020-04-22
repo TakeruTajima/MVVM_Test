80d452fd069a7b906337c6992d4a3c84592bad59
home <mr2o527@gmail.com>
2020-04-22 21:52:35 +0900

    2020/4/22

--- app/src/main/java/com/mr2/mvvm_test/ui/dagger_sample/MainActivity.java
+++ app/src/main/java/com/mr2/mvvm_test/ui/dagger_sample/MainActivity.java
@@ -12,6 +12,20 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 
+/**
+ * link: https://satoshun.github.io/2015/05/dagger2/
+ * .@Inject: 依存を注入します.
+ * .@Module: インスタンスをProvideするメソッド郡を定義します.
+ * .@Provide; 依存を解決するためのインスタンスを提供します.
+ * .@Component: @Injectと@Module間の関係を定義します.
+ *
+ * 基本的なDagger2の使い方をまとめると
+ * .@Injectで, 依存関係を定義する
+ * .@Module, @Providesで依存関係を解決するようにインスタンスを提供する
+ * .@Componentで複数のModuleを用いて, 依存解決Graphオブジェクトを生成する
+ * アプリで依存解決Graphを使い, DIを行う
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     private CoffeeShop coffeeShop;
